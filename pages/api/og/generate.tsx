df5565a4b805ea60a16b1a76d5f9802ea282e696
truongnguyenptn <truongnguyen310.work@gmail.com>
2024-09-04 17:43:32 +0700

    feat: init

--- pages/api/og/generate.tsx
+++ pages/api/og/generate.tsx
@@ -51,122 +51,109 @@ export default async function GenerateOgImage(request: NextRequest) {
 
   const questionClass = question.length > 90 ? "text-4xl" : "text-5xl";
 
-  const [boldFont, regularFont, bg, zeitgeistBadge] = await Promise.all([
-    fetch(
-      new URL(
-        "../../../public/fonts/inter/static/Inter-Bold.ttf",
-        import.meta.url,
-      ).href,
-    ).then((res) => res.arrayBuffer()),
-    fetch(
-      new URL(
-        "../../../public/fonts/inter/static/Inter-Regular.ttf",
-        import.meta.url,
-      ).href,
-    ).then((res) => res.arrayBuffer()),
-    fetch(new URL("../../../public/og/bg1.png", import.meta.url)).then((res) =>
-      res.arrayBuffer(),
-    ),
-    fetch(
-      new URL("../../../public/og/zeitgeist_badge.png", import.meta.url),
-    ).then((res) => res.arrayBuffer()),
-  ]);
-  const image = (
-    <div
-      tw="px-16 pt-16 pb-24 text-white"
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-      }}
-    >
-      <img
-        src={bg as any}
-        tw="absolute top-0 left-0"
-        style={{
-          width: 1200,
-          height: 675,
-          objectFit: "cover",
-        }}
-      />
-      <div tw="flex flex-col h-full w-full">
-        <div tw="flex">
-          <img
-            style={{
-              width: 150,
-              height: 150,
-              objectFit: "cover",
-            }}
-            src={cmsImageUrl ?? fallbackImagePath}
-            tw="rounded-[5px]"
-          />
-          <h1 tw={`${questionClass} ml-6`} style={{ lineHeight: "1.3em" }}>
-            {question}
-          </h1>
-        </div>
-        <div tw="flex flex-col mt-10">
-          <h2 tw={`font-bold text-4xl font-sans`}>
-            {market.status === "Reported" || market.status === "Resolved"
-              ? "Winning Outcome:"
-              : "Prediction:"}
-          </h2>
-          <div tw={`font-semibold ${"text-6xl"} `} style={{ color: "#ABC1F9" }}>
-            {prediction.name != null && prediction.name !== ""
-              ? market.marketType.categorical
-                ? `${prediction.name} (${prediction.percentage}%)`
-                : `${Intl.NumberFormat("en-US", {
-                    maximumSignificantDigits: 3,
-                  }).format(Number(prediction.name))}`
-              : "No Prediction"}
-          </div>
-        </div>
-        <div tw="flex mt-auto w-full items-center">
-          <div tw="flex flex-col mr-34">
-            <h2 tw={`font-bold ${"text-3xl"} font-sans`}>Ends:</h2>
-            <div tw="text-4xl -mt-1" style={{ color: "#ABC1F9" }}>
-              {ends}
-            </div>
-          </div>
-          {Number(volume) > 0 && (
-            <div tw="flex flex-col">
-              <h2 tw={`font-bold ${"text-3xl"} font-sans`}>Volume:</h2>
-              <div
-                tw={`flex ${"text-4xl"}  -mt-1`}
-                style={{ color: "#ABC1F9" }}
-              >
-                {formatNumberCompact(Number(volume))} {currencyMetadata?.name}
-              </div>
-            </div>
-          )}
-          <div tw="flex ml-auto mt-4">
-            <img
-              style={{
-                width: 250,
-              }}
-              src={zeitgeistBadge as any}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  // const [boldFont, regularFont, bg, zeitgeistBadge] = await Promise.all([
+  //   fetch(
+  //     new URL(
+  //       "../../../public/fonts/inter/static/Inter-Bold.ttf",
+  //       import.meta.url,
+  //     ).href,
+  //   ).then((res) => res.arrayBuffer()),
+  //   fetch(
+  //     new URL(
+  //       "../../../public/fonts/inter/static/Inter-Regular.ttf",
+  //       import.meta.url,
+  //     ).href,
+  //   ).then((res) => res.arrayBuffer()),
+  //   fetch(new URL("../../../public/og/bg1.png", import.meta.url)).then((res) =>
+  //     res.arrayBuffer(),
+  //   ),
+  //   fetch(
+  //     new URL("../../../public/og/zeitgeist_badge.png", import.meta.url),
+  //   ).then((res) => res.arrayBuffer()),
+  // ]);
+  // const image = (
+  //   <div
+  //     tw="px-16 pt-16 pb-24 text-white"
+  //     style={{
+  //       width: "100%",
+  //       height: "100%",
+  //       display: "flex",
+  //     }}
+  //   >
+  //     <img
+  //       src={bg as any}
+  //       tw="absolute top-0 left-0"
+  //       style={{
+  //         width: 1200,
+  //         height: 675,
+  //         objectFit: "cover",
+  //       }}
+  //     />
+  //     <div tw="flex flex-col h-full w-full">
+  //       <div tw="flex">
+  //         <img
+  //           style={{
+  //             width: 150,
+  //             height: 150,
+  //             objectFit: "cover",
+  //           }}
+  //           src={cmsImageUrl ?? fallbackImagePath}
+  //           tw="rounded-[5px]"
+  //         />
+  //         <h1 tw={`${questionClass} ml-6`} style={{ lineHeight: "1.3em" }}>
+  //           {question}
+  //         </h1>
+  //       </div>
+  //       <div tw="flex flex-col mt-10">
+  //         <h2 tw={`font-bold text-4xl font-sans`}>
+  //           {market.status === "Reported" || market.status === "Resolved"
+  //             ? "Winning Outcome:"
+  //             : "Prediction:"}
+  //         </h2>
+  //         <div tw={`font-semibold ${"text-6xl"} `} style={{ color: "#ABC1F9" }}>
+  //           {prediction.name != null && prediction.name !== ""
+  //             ? market.marketType.categorical
+  //               ? `${prediction.name} (${prediction.percentage}%)`
+  //               : `${Intl.NumberFormat("en-US", {
+  //                   maximumSignificantDigits: 3,
+  //                 }).format(Number(prediction.name))}`
+  //             : "No Prediction"}
+  //         </div>
+  //       </div>
+  //       <div tw="flex mt-auto w-full items-center">
+  //         <div tw="flex flex-col mr-34">
+  //           <h2 tw={`font-bold ${"text-3xl"} font-sans`}>Ends:</h2>
+  //           <div tw="text-4xl -mt-1" style={{ color: "#ABC1F9" }}>
+  //             {ends}
+  //           </div>
+  //         </div>
+  //         {Number(volume) > 0 && (
+  //           <div tw="flex flex-col">
+  //             <h2 tw={`font-bold ${"text-3xl"} font-sans`}>Volume:</h2>
+  //             <div
+  //               tw={`flex ${"text-4xl"}  -mt-1`}
+  //               style={{ color: "#ABC1F9" }}
+  //             >
+  //               {formatNumberCompact(Number(volume))} {currencyMetadata?.name}
+  //             </div>
+  //           </div>
+  //         )}
+  //         <div tw="flex ml-auto mt-4">
+  //           <img
+  //             style={{
+  //               width: 250,
+  //             }}
+  //             src={zeitgeistBadge as any}
+  //           />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 
+  const image = <div>Dehype</div>;
   return new ImageResponse(image, {
     width: 1200,
     height: 630,
-    fonts: [
-      {
-        name: "Inter",
-        data: regularFont,
-        weight: 400,
-        style: "normal",
-      },
-      {
-        name: "Inter",
-        data: boldFont,
-        weight: 700,
-        style: "normal",
-      },
-    ],
   });
 }
